0da36bd373ac501f00c058ed93113b7bb4f5d743
ekintuncok <tuncokekin@gmail.com>
2024-07-21 12:32:14 -0700

    plot scripts

---
+++ code/fig_2_B.R
@@ -0,0 +1,125 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(plotrix)
+# library(export) # UNCOMMENT IF SAVING THE PRODUCED PLOT! 
+library(superb)
+library(rstatix)
+
+directory = "/Volumes/purplab/EXPERIMENTS/1_Current_Experiments/Ekin/monkeyPF"
+setwd(directory)
+
+# comparing Asymmetry ratio or dprime data:
+file_name = "data"
+location_info = "cardinals"
+
+# Load the data
+data_tbl = read.csv((sprintf("%s/data_to_analyze/%s_botheyes_%s.csv", directory, file_name, location_info)), header = T)
+
+# Convert to long format
+data_tbl <- data_tbl %>%
+  gather(key = "location", value = "sensitivity", UVM, LVM, LHM, RHM) %>%
+  mutate(id = as.factor(id))  # Ensure id is treated as a factor
+
+# Calculate the mean sensitivity for each observer (id)
+observer_means <- data_tbl %>%
+  group_by(group, id) %>%
+  summarise(mean_sensitivity = mean(sensitivity, na.rm = TRUE))
+
+# get grand mean
+grand_means <- data_tbl %>%
+  group_by(group) %>%
+  summarise(mean_sensitivity = mean(sensitivity, na.rm = TRUE))
+
+# Step 3: Join the observer means with the original data
+data_corrected <- left_join(data_tbl, observer_means, by = c("group", "id"))
+
+# Join the grand means with the corrected data
+data_corrected <- data_tbl %>%
+  left_join(observer_means, by = c("id", "group")) %>%
+  left_join(grand_means, by = "group")
+
+colnames(data_corrected)[colnames(data_corrected) == "mean_sensitivity.x"] <- "observer_means"
+colnames(data_corrected)[colnames(data_corrected) == "mean_sensitivity.y"] <- "grand_means"
+
+data_corrected <- data_corrected %>% mutate(corrected_sensitivity = sensitivity - observer_means + grand_means)
+
+# Calculate the number of subjects in each group
+nSubj <- data_corrected %>%
+  group_by(group) %>%
+  summarise(n = n_distinct(id))
+
+# Calculate the standard deviation for each group and location
+std_dev <- data_corrected %>%
+  group_by(group, location) %>%
+  summarise(std = sd(corrected_sensitivity, na.rm = TRUE))
+
+# Merge to get the number of subjects for each group
+std_dev <- std_dev %>%
+  left_join(nSubj, by = "group")
+
+# Calculate the standard error of the mean (SEM)
+std_dev <- std_dev %>%
+  mutate(sem = std / sqrt(n))
+
+# Apply Morey's correction
+numConditions <- 4
+M <- sqrt(numConditions / (numConditions - 1))
+std_dev <- std_dev %>%
+  mutate(corrected_sem = sem * M)
+
+# Select the relevant columns for the final output
+sem_data <- std_dev %>%
+  select(group, location, corrected_sem)
+
+# now load the normalized data that will be plotted:
+file_name = "normalized_data"
+
+# Load the data
+data_tbl = read.csv((sprintf("%s/data_to_analyze/%s_botheyes_%s.csv", directory, file_name, location_info)), header = T)
+data_tbl <- data_tbl %>%
+  gather(key = "location", value = "sensitivity", UVM, LVM, LHM, RHM)
+data_tbl$location <- factor(data_tbl$location, levels = c('UVM', 'LVM', 'LHM', 'RHM'))
+
+library(plyr)
+data_summary <- function(data, varname, groupnames) {
+  summary_func <- function(x, col) {
+    c(mean = mean(x[[col]], na.rm = TRUE),
+      sem = std.error(x[[col]], na.rm = TRUE))
+  }
+  data_sum <- ddply(data, groupnames, .fun = summary_func, varname)
+  colnames(data_sum)[which(colnames(data_sum) == "mean")] <- varname
+  return(data_sum)
+}
+
+df2 <- data_summary(data_tbl, varname = "sensitivity", groupnames = c("group", "location"))
+df2 <- as_tibble(df2)
+
+
+df2$location <- as.factor(df2$location)
+df2$group <- as.factor(df2$group)
+head(df2)
+
+sem_data$group <- as.factor(sem_data$group)
+sem_data$location <- as.factor(sem_data$location)
+
+# Perform the join to update the sem values with corrected_sem
+df2_updated <- df2 %>%
+  left_join(sem_data, by = c("group", "location")) %>%
+  mutate(sem = corrected_sem) %>%
+  select(-corrected_sem)
+
+# Default bar plot
+p <- ggplot(df2_updated, aes(x = location, y = sensitivity, fill = group, color = as.factor(group), shape = as.factor(group))) +
+  geom_bar(stat = "identity", color = "black", position = position_dodge()) +
+  geom_errorbar(aes(ymin = sensitivity - sem, ymax = sensitivity + sem), width = 0.3, position = position_dodge(1))
+
+p + labs(x = "location", y = "sensitivity") +
+  theme_classic() +
+  scale_fill_manual(values = c('black', 'red'))
+
+
+print(p)
+
+# Save the plot to a PowerPoint file
+#graph2office(file = "fig_1_B.pptx", width = 5, height = 3)
---
+++ code/fig_3_A.R
@@ -0,0 +1,210 @@
+library(plotrix)  # Load the plotrix library for polar plots
+library(dplyr)
+library(plyr)  # For data manipulation
+library(export)
+library(boot)
+library(reshape2)
+library(ggplot2)
+library(cowplot)
+
+
+directory = "/Volumes/purplab/EXPERIMENTS/1_Current_Experiments/Ekin/monkeyPF/analysis_code"
+setwd(directory)
+source(paste0(directory, "/s1_monkey_analysis.R"))
+source(paste0(directory, "/s1_human_analysis.R"))
+
+monkey_fellow_data <- monkey.dprime[1,,]
+monkey_amb_data <- monkey.dprime[2,,]
+
+monkey.amb.norm <- monkey_amb_data 
+monkey.fellow.norm <- monkey_fellow_data
+
+monkeyamb = apply(monkey.amb.norm, 2, mean)
+monkeyfellow = apply(monkey.fellow.norm, 2, mean)
+
+# Add error bars for monkeys
+sem_monkey_amb = std.error(monkey_amb_data);
+sem_monkey_fellow = std.error(monkey_fellow_data);
+
+# Define the angles
+angle_pos <- c(0, 45, 90, 135, 180, 225, 270, 315, 360)
+angles_rad <- angle_pos * pi / 180
+
+par(mfrow = c(2, 1))
+
+# Plot for monkeys
+polar.plot(c(monkeyamb, monkeyamb[1]),angle_pos, lwd = 3, lty = 1, line.col = '#4A7056',
+           point.col = '#4A7056', 
+           point.symbols = 1, cex = 1, show.grid = TRUE, rp.type = "spline", 
+           labels = c(0, 45, 90, 135, 180, 225, 270, 315), show.grid.labels = 0, radial.lim=c(0,3))
+
+
+for (i in seq_along(angle_pos)) {
+  x <- c(monkeyamb, monkeyamb[1])[i] * cos(angles_rad[i])
+  y <- c(monkeyamb, monkeyamb[1])[i] * sin(angles_rad[i])
+  dx <- sem_monkey_amb[i] * cos(angles_rad[i])
+  dy <- sem_monkey_amb[i] * sin(angles_rad[i])
+  
+  # Offset starting and ending points of error bars
+  x_start <- x - dx/2
+  y_start <- y - dy/2
+  x_end <- x + dx/2
+  y_end <- y + dy/2
+  
+  arrows(x_start, y_start, x_end, y_end, angle = 90, code = 3, length = 0, col = '#4A7056', lwd = 4)
+}
+par(new=T)
+polar.plot(c(monkeyfellow, monkeyfellow[1]),angle_pos, lwd = 3, lty = 1, line.col = '#807DBA',
+           point.col = '#807DBA', 
+           point.symbols = 1, cex = 1, show.grid = TRUE, rp.type = "spline", 
+           labels = c(0, 45, 90, 135, 180, 225, 270, 315), show.grid.labels = 1, radial.lim=c(0,3))
+
+
+for (i in seq_along(angle_pos)) {
+  x <- c(monkeyfellow, monkeyfellow[1])[i] * cos(angles_rad[i])
+  y <- c(monkeyfellow, monkeyfellow[1])[i] * sin(angles_rad[i])
+  dx <- sem_monkey_fellow[i] * cos(angles_rad[i])
+  dy <- sem_monkey_fellow[i] * sin(angles_rad[i])
+  
+  # Offset starting and ending points of error bars
+  x_start <- x - dx/2
+  y_start <- y - dy/2
+  x_end <- x + dx/2
+  y_end <- y + dy/2
+  
+  arrows(x_start, y_start, x_end, y_end, angle = 90, code = 3, length = 0, col = '#807DBA', lwd = 4)
+}
+
+
+#graph2ppt(file="supp_1_A_monkeytwoeyes_dprime.pptx", width=5, height=5) 
+
+# look at the RT data
+monkeyrt_across_att <- apply(monkey.reaction_time, c(1, 3, 4), mean)
+monkey_fellow_data <- monkeyrt_across_att[1,,]
+monkey_amb_data <- monkeyrt_across_att[2,,]
+
+monkey.amb.norm <- monkey_amb_data 
+monkey.fellow.norm <- monkey_fellow_data 
+
+monkeyamb = apply(monkey.amb.norm, 2, mean)
+monkeyfellow = apply(monkey.fellow.norm, 2, mean)
+
+# Add error bars for monkeys
+sem_monkey_amb = std.error(monkey_amb_data);
+sem_monkey_fellow = std.error(monkey_fellow_data);
+
+# Define the angles
+angle_pos <- c(0, 45, 90, 135, 180, 225, 270, 315, 360)
+angles_rad <- angle_pos * pi / 180
+
+# Plot for monkeys
+polar.plot(c(monkeyamb, monkeyamb[1]),angle_pos, lwd = 3, lty = 1, line.col = '#4A7056',
+           point.col = '#4A7056', 
+           point.symbols = 1, cex = 1, show.grid = TRUE, rp.type = "spline", 
+           labels = c(0, 45, 90, 135, 180, 225, 270, 315), show.grid.labels = 0, radial.lim=c(0,900))
+
+
+for (i in seq_along(angle_pos)) {
+  x <- c(monkeyamb, monkeyamb[1])[i] * cos(angles_rad[i])
+  y <- c(monkeyamb, monkeyamb[1])[i] * sin(angles_rad[i])
+  dx <- sem_monkey_amb[i] * cos(angles_rad[i])
+  dy <- sem_monkey_amb[i] * sin(angles_rad[i])
+  
+  # Offset starting and ending points of error bars
+  x_start <- x - dx/2
+  y_start <- y - dy/2
+  x_end <- x + dx/2
+  y_end <- y + dy/2
+  
+  arrows(x_start, y_start, x_end, y_end, angle = 90, code = 3, length = 0, col = '#4A7056', lwd = 4)
+}
+par(new=T)
+polar.plot(c(monkeyfellow, monkeyfellow[1]),angle_pos, lwd = 3, lty = 1, line.col = '#807DBA',
+           point.col = '#807DBA', 
+           point.symbols = 1, cex = 1, show.grid = TRUE, rp.type = "spline", 
+           labels = c(0, 45, 90, 135, 180, 225, 270, 315), show.grid.labels = 1, radial.lim=c(0,900))
+
+
+for (i in seq_along(angle_pos)) {
+  x <- c(monkeyfellow, monkeyfellow[1])[i] * cos(angles_rad[i])
+  y <- c(monkeyfellow, monkeyfellow[1])[i] * sin(angles_rad[i])
+  dx <- sem_monkey_fellow[i] * cos(angles_rad[i])
+  dy <- sem_monkey_fellow[i] * sin(angles_rad[i])
+  
+  # Offset starting and ending points of error bars
+  x_start <- x - dx/2
+  y_start <- y - dy/2
+  x_end <- x + dx/2
+  y_end <- y + dy/2
+  
+  arrows(x_start, y_start, x_end, y_end, angle = 90, code = 3, length = 0, col = '#807DBA', lwd = 4)
+}
+
+#graph2ppt(file="supp_1_A_monkeytwoeyes_RT.pptx", width=5, height=5) 
+
+# Fig 3 A: Bar plots
+# Convert the matrix to a data frame
+monkey_fellow_data <- monkey.dprime[1,,]
+monkey_amb_data <- monkey.dprime[2,,]
+monkey_amb_data_df <- as.data.frame(monkey_amb_data)
+monkey_fellow_data_df <- as.data.frame(monkey_fellow_data)
+
+# Name the columns
+colnames(monkey_amb_data_df) <- c("RHM","IC_NE","UVM","IC_NW","LHM","IC_SW", "LVM","IC_SE")
+colnames(monkey_fellow_data_df) <- c("RHM","IC_NE","UVM","IC_NW","LHM","IC_SW", "LVM","IC_SE")
+
+# Select the columns to plot
+columns_to_plot <- c("RHM","LHM","UVM","LVM")
+
+# Reshape the data for plotting
+monkey_amb_data_melt <- melt(monkey_amb_data_df[, columns_to_plot], variable.name = "Condition", value.name = "Value")
+monkey_fellow_data_melt <- melt(monkey_fellow_data_df[, columns_to_plot], variable.name = "Condition", value.name = "Value")
+
+# Set the order of the levels
+order_levels <- c("UVM", "LVM", "LHM", "RHM")
+monkey_amb_data_melt$Condition <- factor(monkey_amb_data_melt$Condition, levels = order_levels)
+monkey_fellow_data_melt$Condition <- factor(monkey_fellow_data_melt$Condition, levels = order_levels)
+
+# Calculate the mean and SEM for each condition
+monkey_amb_summary <- monkey_amb_data_melt %>%
+  group_by(Condition) %>%
+  dplyr::summarize(Mean = mean(Value), SEM = sd(Value) / sqrt(length(Value)))
+
+monkey_fellow_summary <- monkey_fellow_data_melt %>%
+  group_by(Condition) %>%
+  dplyr::summarize(Mean = mean(Value), SEM = sd(Value) / sqrt(length(Value)))
+
+# Create bar plots with error bars
+plot_amb <- ggplot(monkey_amb_summary, aes(x=Condition, y=Mean, fill=Condition)) +
+  geom_bar(stat="identity") +
+  geom_errorbar(aes(ymin=Mean-SEM, ymax=Mean+SEM), width=0.2) +
+  labs(title="Monkey Amb Data", x="Condition", y="Mean Value") +
+  ylim(0, 3) +
+  theme_minimal() +
+  theme(axis.line = element_line(color = "black"),
+        axis.ticks = element_line(color = "black"),
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10))
+
+plot_fellow <- ggplot(monkey_fellow_summary, aes(x=Condition, y=Mean, fill=Condition)) +
+  geom_bar(stat="identity") +
+  geom_errorbar(aes(ymin=Mean-SEM, ymax=Mean+SEM), width=0.2) +
+  labs(title="Monkey Fellow Data", x="Condition", y="Mean Value") +
+  ylim(0, 3) +
+  theme_minimal() +
+  theme(axis.line = element_line(color = "black"),
+        axis.ticks = element_line(color = "black"),
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10))
+
+# Combine plots into one figure
+combined_plot <- plot_grid(
+  plot_amb + ggtitle("Panel A: Monkey Amb Data"),
+  plot_fellow + ggtitle("Panel B: Monkey Fellow Data"),
+  labels = c("A", "B"),
+  ncol = 2
+)
+
+# Print the combined plot
+print(combined_plot)
+
